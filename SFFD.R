f5d2edea279879a7d9fff14f745282cf4cac9e33
Nicholas Coxen <nicjcoxen@gmail.com>
2022-04-10 15:29:27 +1200

    csv

--- SFFD.R
+++ SFFD.R
@@ -16,9 +16,9 @@ SFFD <- fires %>% select(c(4,5,11,16,17,18,19,24,28,32))
 SFFD
 
 df<- SFFD %>% rename(Zipcode = 'Zipcode of Incident',Response_Time = 'On Scene DtTm', Neighborhood = 'Neighborhooods - Analysis Boundaries', Priority = 'Final Priority') %>%
-  mutate(Year = substring(SFFD$`Call Date`,7,10),Month = substring(SFFD$`Call Date`,1,2) ) %>%
-  arrange(desc(Year), desc(Month))
+  mutate(Year = substring(SFFD$`Call Date`,7,10),Month = substring(SFFD$`Call Date`,1,2), Day = substring(SFFD$`Call Date`,4,5) ) %>%
+  arrange(desc(Year), desc(Month), desc(Day))
 
-#write.csv(df,"San Francisco Fire Dept.csv",row.names=FALSE)
+write.csv( df,"San Francisco Fire Dept.csv",row.names=FALSE)
 
 
